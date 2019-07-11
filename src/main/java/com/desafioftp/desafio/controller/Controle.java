3bf5fc554ecd46d4cd41a64961d422a74813e588
Daniel Rodrigues <infantariamilitar@gmail.com>
2019-07-11 13:08:57 -0300

    Swagger

--- src/main/java/com/desafioftp/desafio/controller/Controle.java
+++ src/main/java/com/desafioftp/desafio/controller/Controle.java
@@ -1,5 +1,11 @@
 package com.desafioftp.desafio.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Api(value = "Usuario")
 public class Controle {
 
 }
