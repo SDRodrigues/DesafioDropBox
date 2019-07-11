4d185064ad2157535b90c20e3045537decf917c7
Daniel Rodrigues <infantariamilitar@gmail.com>
2019-07-11 15:34:51 -0300

    controller swagger

--- src/main/java/com/desafioftp/desafio/SwaggerConfig.java
+++ src/main/java/com/desafioftp/desafio/SwaggerConfig.java
@@ -1,11 +1,7 @@
 package com.desafioftp.desafio;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,9 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-
     public Docket desafioDropBox() {
-
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.
--- src/main/java/com/desafioftp/desafio/controller/Controle.java
+++ src/main/java/com/desafioftp/desafio/controller/Controle.java
@@ -1,14 +1,13 @@
 package com.desafioftp.desafio.controller;
 
 import com.desafioftp.desafio.model.Usuario;
-import com.desafioftp.desafio.repository.Repositorio;
+import com.desafioftp.desafio.service.Servico;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,24 +16,60 @@ import java.util.List;
 @Api(value = "Usuario")
 public class Controle {
 
-    @Autowired
-    private Repositorio repositorio;
+    private Servico servico;
 
+    @Autowired
+    public Controle(Servico servico) {
+        this.servico = servico;
+    }
 
     @GetMapping(value="/model", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     @ApiOperation(value="Buscar usuário", response= Usuario.class, notes="Essa operação busca o usuario.")
     @ApiResponses(value= {
             @ApiResponse(code=200, message="Retorna um Usuario com uma mensagem de sucesso", response=Usuario.class),
+            @ApiResponse(code=404, message = "Não encontrou usuário", response = Usuario.class),
             @ApiResponse(code=500, message="Não conseguiu buscar usuário", response=Usuario.class)
     })
     public List<Usuario> consultar(){
-        return this.repositorio.findAll();
+        return this.servico.lerUsuario();
     }
 
 
+    @PostMapping(value = "/model")
+    @ResponseBody
+    @ApiOperation(value="Buscar usuário", response= Usuario.class, notes="Essa operação busca o usuario.")
+    @ApiResponses(value= {
+            @ApiResponse(code=201, message="Criou um usuário com sucesso", response=Usuario.class),
+            @ApiResponse(code=404, message = "Não encontrou usuário", response = Usuario.class),
+            @ApiResponse(code=500, message="Não conseguiu buscar usuário", response=Usuario.class)
+    })
+    public Usuario criaUsuario(Usuario usuario) {
+        return servico.criarUsuario(usuario);
+    }
 
 
+    @PutMapping(value = "/model")
+    @ResponseBody
+    @ApiOperation(value="Buscar usuário", response= Usuario.class, notes="Essa operação busca o usuario.")
+    @ApiResponses(value= {
+            @ApiResponse(code=200, message="Retorna um Usuario com uma mensagem de sucesso", response=Usuario.class),
+            @ApiResponse(code=404, message = "Não encontrou usuário", response = Usuario.class),
+            @ApiResponse(code=500, message="Não conseguiu buscar usuário", response=Usuario.class)
+    })
+    public Usuario editaUsuario(Integer id, Usuario usuario) {
+        return servico.editaUsuario(id, usuario);
+    }
 
+    @DeleteMapping(value = "/model")
+    @ResponseBody
+    @ApiOperation(value="Buscar usuário", response= Usuario.class, notes="Essa operação busca o usuario.")
+    @ApiResponses(value= {
+            @ApiResponse(code=200, message="Excluiu o usuario", response=Usuario.class),
+            @ApiResponse(code=500, message="Não conseguiu buscar usuário", response=Usuario.class)
+    })
+    public void deletaUsuario(Usuario usuario) {
+         servico.deletaUsuario(usuario);
+    }
 
 }
